dc2ffb49bcab3ee21cb7a0f638c53bbe3eacc76f
rycont <reactdev@kakao.com>
2022-02-01 13:53:49 +0900

    refactor: remove top-level async

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,32 @@
 import * as ZXing from "@zxing/library";
 
-const codeReader = new ZXing.BrowserMultiFormatReader();
-const videoInputDevices = await codeReader.listVideoInputDevices();
-const selectedDeviceId =
-  videoInputDevices[videoInputDevices.length - 1].deviceId;
+async function main() {
+  const codeReader = new ZXing.BrowserMultiFormatReader();
+  const videoInputDevices = await codeReader.listVideoInputDevices();
+  const selectedDeviceId =
+    videoInputDevices[videoInputDevices.length - 1].deviceId;
 
-codeReader.decodeFromVideoDevice(selectedDeviceId, "video", (result) => {
-  if (result) {
-    console.log("ABB");
-    createAlert(result.getText());
-  }
-});
+  codeReader.decodeFromVideoDevice(selectedDeviceId, "video", (result) => {
+    if (result) {
+      console.log("ABB");
+      createAlert(result.getText());
+    }
+  });
 
-function createAlert(text: string) {
-  fetch(`https://pubsub.rycont.ninja/pub/scanner/${text}`);
+  function createAlert(text: string) {
+    fetch(`https://pubsub.rycont.ninja/pub/scanner/${text}`);
 
-  const alertElement = document.createElement("div");
-  alertElement.classList.add("alert");
-  alertElement.appendChild(document.createTextNode(text));
-  document.body.appendChild(alertElement);
-  setTimeout(() => {
-    alertElement.classList.add("disappear");
+    const alertElement = document.createElement("div");
+    alertElement.classList.add("alert");
+    alertElement.appendChild(document.createTextNode(text));
+    document.body.appendChild(alertElement);
     setTimeout(() => {
-      alertElement.remove();
+      alertElement.classList.add("disappear");
+      setTimeout(() => {
+        alertElement.remove();
+      }, 1000);
     }, 1000);
-  }, 1000);
+  }
 }
+
+main();
